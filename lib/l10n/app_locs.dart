ab777087829b5192bded6836dc4da8c12a676883
Steve Gehrman <sgehrman@gmail.com>
2024-10-25 19:55:02 -0700

    sdf

--- lib/l10n/app_locs.dart
+++ lib/l10n/app_locs.dart
@@ -1,3 +1,4 @@
 import 'package:dfc_flutter/l10n/app_localizations.dart';
 
 const deckrHelpLocDelegates = AppLocalizations.localizationsDelegates;
+const deckrHelpLocales = AppLocalizations.supportedLocales;
